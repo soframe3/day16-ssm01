6d86820d1d878a899f97e6a7d5007f4cb5526349
赵劲 <479952026@qq.com>
2021-05-24 17:45:06 +0800

    TestDemo.java 第二次提交

--- src/main/java/cn/kgc/test/TestDemo.java
+++ src/main/java/cn/kgc/test/TestDemo.java
@@ -3,5 +3,7 @@ package cn.kgc.test;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("groupA组开发人员提交代码...");
+
+        System.out.println("groupA组开发人员提交代码22222...");
     }
 }
